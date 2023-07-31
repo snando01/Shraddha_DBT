a7604a47f67379d2e1413dc28c17bdb840068d5c
Shraddha Nandoskar <snando01@kenvue.com>
2023-07-31 15:57:30 +0000

    commit

--- macros/Config_scripts/Database_change.sql
+++ macros/Config_scripts/Database_change.sql
@@ -1,13 +1,13 @@
 {% macro generate_database_name(custom_database_name, node) -%}
 
     {%- set default_database = target.database -%}
-    {%- if custom_database_name is none -%}
-
+    {%- if custom_database_name is none -%} 
+    
         {{ default_database }}
 
-    {%- else -%}
-
-         {{ custom_database_name | trim }}
+    {%- else -%} 
+    
+        {{ custom_database_name | trim }}
 
     {%- endif -%}
 
